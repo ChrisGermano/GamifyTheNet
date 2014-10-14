1e02f2c640e5a697a49e706a6ee11bab1e45ab83
ChrisGermano <chris.eg.germano@gmail.com>
2014-10-14 10:57:36 -0400

    Fixed level system, finished for 5 levels

--- extension/js/game_actions.js
+++ extension/js/game_actions.js
@@ -1,21 +1,22 @@
 //Popup script
 
 $(document).ready(function() {
-  chrome.storage.local.get("title", function(result) {
-    $(".pt").text(result.title);
+  chrome.storage.local.get('title', function(result) {
+    $('.pt').text(result.title);
   });
-  chrome.storage.local.get("level", function(result) {
-    $(".pl").text(result.level);
+  chrome.storage.local.get('level', function(result) {
+    $('.pl').text(result.level);
     setBadge(result.level);
     var mxp = 2 * Math.pow(parseInt(result.level), 3);
-    $(".mxp").text(mxp);
+    $('.mxp').text(mxp);
   });
-  chrome.storage.local.get("xp", function(result) {
-    $(".cxp").text(result.xp);
+  chrome.storage.local.get('xp', function(result) {
+    $('.cxp').text(result.xp);
   });
 
 });
 
 function setBadge(level) {
-  $(".icon_container img").attr("src", "icons/ranks/Tier" + (parseInt(level)-1) + ".png");
+  var iconL = parseInt(level)%6;
+  $('.icon_container img').attr('src', 'icons/ranks/Tier' + (parseInt(level)%5) + '.png');
 }
--- extension/js/user_actions.js
+++ extension/js/user_actions.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   chrome.runtime.sendMessage({
-    action: "GainXP",
+    action: 'GainXP',
     value: 1
   });
 
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $(document).keyup(function(e) {
     if (e.keyCode == 27) {
       chrome.runtime.sendMessage({
-        action: "Nuke"
+        action: 'Nuke'
       });
     }
   });
@@ -20,75 +20,75 @@ $(document).ready(function() {
   //Tweet
   $('.tweet-btn').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 8
     });
   });
 
   //Twitter follow
-  $(".follow-text").click(function() {
+  $('.follow-text').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 4
     });
   });
 
   //
-  $("#sharelink_").submit(function() {
+  $('#sharelink_').submit(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 25
     });
   });
 
   //Youtube like
-  $("#watch-like").click(function() {
+  $('#watch-like').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 2
     });
   });
 
   //Youtube comment
-  $(".d-k-l").click(function() {
+  $('.d-k-l').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 6
     });
   });
 
   //Facebook status
-  $("._42ft").click(function() {
-    if ($(".mentionsHidden").value().length > 0) {
+  $('._42ft').click(function() {
+    if ($('.mentionsHidden').value().length > 0) {
       chrome.runtime.sendMessage({
-        action: "GainXP",
+        action: 'GainXP',
         value: 14
       });
     }
   });
 
   //Tumblr post
-  $(".create_post_button").click(function() {
+  $('.create_post_button').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 20
     });
   });
 
   //Pinterest repin
-  $(".repinSmall").click(function() {
+  $('.repinSmall').click(function() {
     chrome.runtime.sendMessage({
-      action: "GainXP",
+      action: 'GainXP',
       value: 5
     });
   });
 
   //Once the user moves the mouse, it initializes the game session if unitialized
-  $("body").mousemove(function() {
+  $('body').mousemove(function() {
     if (session_init == 0) {
       session_init = 1;
       chrome.runtime.sendMessage({
-        action: "Load"
+        action: 'Load'
       });
     }
   });
